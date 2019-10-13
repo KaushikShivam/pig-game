1cb3d8cf79c7faf0b12d5dc8abe43bf2fd3fdd4e
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-13 23:33:29 +0530

    Finish custom onput

--- app.js
+++ app.js
@@ -40,7 +40,16 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		scores[activePlayer] += roundScore;
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-		if (scores[activePlayer] >= 100) {
+		var input = document.querySelector('.final-score').value;
+
+		var winnerScore;
+		if (input) {
+			winnerScore = input;
+		} else {
+			winnerScore = 100;
+		}
+
+		if (scores[activePlayer] >= winnerScore) {
 			document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
 			document.querySelector('.dice').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
