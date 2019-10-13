2c489a2f646f1b97402b9288550d5f30770e5cf3
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-13 23:02:59 +0530

    Update UI for game winner

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 	if (scores[activePlayer] >= 20) {
 		document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
 		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 	} else {
 		nextPlayer();
 	}
